f7ff6305f6cd7af1b77fa42193007509bf586b3a
gmemstr <gabriel@gitgalaxy.com>
2019-04-21 17:12:09 -0700

    Refactor create server for smaller footprint, createlocation function.

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"github.com/gmemstr/nginx/provision"
-	"os"
 )
 
 func main() {
-	provision.ProvisionServer(os.Args[1])
+	var locations []string
+	location := provision.CreateLocation("/test", false, "", false)
+	location2 := provision.CreateLocation("/test2", true, "http://127.0.0.1:4343", false)
+	location3 := provision.CreateLocation("/test3", true, "http://127.0.0.1:4343", true)
+
+	locations = append(locations, location, location2, location3)
+	provision.CreateServer("example.com", locations)
 }
--- provision/provision.go
+++ provision/provision.go
@@ -2,8 +2,8 @@ package provision
 
 import (
 	"bytes"
-	"html/template"
 	"os"
+	"text/template"
 )
 
 type Server struct {
@@ -23,35 +23,61 @@ type Proxy struct {
 	IsWebsocket  bool
 }
 
-func ProvisionServer(names ...string) {
+// Create server config and write to file.
+func CreateServer(name string, location []string) {
 	tpl := template.Must(template.ParseFiles("templates/server.tpl"))
-	locTpl := template.Must(template.ParseFiles("templates/location.tpl"))
-	for i := 0; i < len(names); i++ {
-		var loc bytes.Buffer
-		resultFile, err := os.Create("nginx-" + names[i])
-		if err != nil {
-			panic(err)
-		}
-		rootLocation := Location{
-			Location: "/",
-			Root: "/var/www/html",
-		}
-		err = locTpl.Execute(&loc, rootLocation)
-		server := Server{
-			ServerName: names[i],
-			LogLocation: "/var/log/" + names[i] + "-nginx.log",
-			Locations: []string{
-				loc.String(),
-			},
-		}
-		err = tpl.Execute(resultFile, server)
-		if err != nil {
-			panic(err)
-		}
+	resultFile, err := os.Create("configs/nginx-" + name)
+	if err != nil {
+		panic(err)
+	}
+
+	server := Server{
+		ServerName:  name,
+		LogLocation: "/var/log/" + name + "-nginx.log",
+		Locations:   location,
+	}
+	err = tpl.Execute(resultFile, server)
+	if err != nil {
+		panic(err)
 	}
 
 }
 
-func ProvisionProxy(name string, proxy Proxy) {
+// Create location string.
+func CreateLocation(name string, proxy bool, proxyAddr string, isWebsocket bool) string {
+	var loc bytes.Buffer
+
+	tplFile, err := template.ParseFiles("templates/location.tpl")
+	if err != nil {
+		panic(err)
+	}
+
+	if proxy {
+		tplFile, err = template.ParseFiles("templates/location_proxy.tpl")
+		if err != nil {
+			panic(err)
+		}
+		location := Proxy{
+			Location:     name,
+			ProxyAddress: proxyAddr,
+			IsWebsocket:  isWebsocket,
+		}
+		tpl := template.Must(tplFile, nil)
+		err = tpl.Execute(&loc, location)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		location := Location{
+			Location: name,
+			Root:     "/var/www/html" + name,
+		}
+		tpl := template.Must(tplFile, nil)
+		err = tpl.Execute(&loc, location)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return loc.String()
 
 }
